84ea8d7663ecb33b19b1facf3f20f297c1e493d5
dhana1236 <dhana081236@gmail.com>
2013-02-08 17:09:45 +0530

    mongoid_rails example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidRailsExample::Application.config.secret_token = '2219ff44d2e819e52005b42bc564bffaee8e1aba977c887dba3cf2f79f1eb706187fb3ae8c68f8f0e4780d5570fff5486b7ad06fb36219a35c5eda4ea085d217'
